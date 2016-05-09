e5898eeeb43bd2e66df40ef5cad65870766a5bfe
Victor Maciel de Oliveira <executnoob@gmail.com>
2016-05-09 11:33:02 -0300

    Commit

--- app/application/controllers/PerfilController.php
+++ app/application/controllers/PerfilController.php
@@ -35,6 +35,20 @@ class PerfilController extends Zend_Controller_Action
 
     }
   }
-
-
+  public function cadastrarAction(){
+    $dados = $this->_getAllParams();
+    $modelUser = new Application_Model_User();
+    $modelUser->gravar($dados);
+    $row = $modelUser->fetchRow('user = "'. $dados['user'] .'" and pass = "'.$dados['pass'].'"');
+    $_SESSION['id_user'] = $row['id_user'];
+    $_SESSION['user'] = $row['user'];
+    $_SESSION['email'] = $row['email'];
+    $_SESSION['nome'] = $row['nome'];
+    $_SESSION['mensagem'] = "<script>$.Notify({
+      caption: 'Logado',
+      content: 'Bem Vindo você foi logado com sucesso',
+      type: 'success'
+    });</script>";
+    $this->redirect('index');
+  }
 }
--- app/application/models/User.php
+++ app/application/models/User.php
@@ -2,4 +2,13 @@
 class Application_Model_User extends Zend_Db_Table
 {
     protected $_name = 'user';
+
+    public function gravar($dados){
+
+      $row = $this->createRow();
+      $row->setFromArray($dados);
+      $row->save();
+
+    }
+
 }
